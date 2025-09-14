ca87e124f00a9ec09bf3c5e3e45f870db149bf4f
Pramod058 <aryalpramod05@gmail.com>
2025-09-14 18:07:05 +0900

    app game

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 import Home from "./components/Home.jsx";
-import Project from "./components/Project.jsx";
-import Contact from "./components/Contact.jsx";
+
+
+
+
 import Certification from "./components/Certification.jsx";
 import Experience from './components/Experience.jsx';
 import "./styles/App.css";
@@ -16,10 +18,10 @@ function App() {
         <main className="content-wrapper">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/project" element={<Project />} />
+
             <Route path="/certification" element={<Certification />} />
             <Route path="/experience" element={<Experience />} />
-            <Route path="/contact" element={<Contact />} />
+
           </Routes>
         </main>
       </div>
